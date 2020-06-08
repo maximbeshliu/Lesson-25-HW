f63ccab1b0c3e17ed324ffaa60f3afd2ec559ce8
Maximbeshliu <maximbeshliu@yahoo.com>
2020-06-08 10:20:37 +0300

    ss

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 
-document.querySelector('.container').classList.add('spinner');
+function Apod(id) {
+    this.id = id;
+    this.date = undefined;
+    let body = document.body;
+    this.container = document.createElement('div');
+    this.container.setAttribute('class', 'container');
+    this.container.setAttribute('id', this.id);
+    this.addElement = function () {
+        if (this.date === undefined) {
+            this.date = new Date();
+            fetch('https://api.nasa.gov/planetary/apod?api_key=Vc2F9ggHHlrgTjgfjgKc9gGhm6Wnp4Hq27Tm0RCm')
+                .then(response => response.json())
+                .then(data => renderPage(data));
 
-fetch('https://api.nasa.gov/planetary/apod?api_key=Vc2F9ggHHlrgTjgfjgKc9gGhm6Wnp4Hq27Tm0RCm')
-    .then(response => response.json())
-    .then(data => renderPage(data));
+        } else {
+            this.date = yyyymmdd();
+            fetch('https://api.nasa.gov/planetary/apod?api_key=Vc2F9ggHHlrgTjgfjgKc9gGhm6Wnp4Hq27Tm0RCm')
+                .then(response => response.json())
+                .then(data => renderPage(data));
+        }
+
+    };
+    this.addElement();
+    body.append(this.container);
+}
+
+const apod = new Apod('apod');
+
+
+function yyyymmdd() {
+    var now = new Date();
+    var y = now.getFullYear();
+    var m = now.getMonth() + 1;
+    var d = now.getDate();
+    var mm = m < 10 ? '0' + m : m;
+    var dd = d < 10 ? '0' + d : d;
+    return '' + y + '-' + mm + '-' + dd;
+}
+
+
+
+///function pictureOfTheDay() {
+
+///document.body.classList.add('spinner');
 
 function renderPage(data) {
-    let body = document.querySelector('.container');
+    let body = document.body;
+
+    let container = document.createElement('div');
+    container.classList.add('container');
+    body.append(container);
 
     let title = document.createElement('h1');
     title.innerText = data.title;
-    body.append(title);
+    container.append(title);
 
     let image = document.createElement('div');
     image.classList.add('image');
@@ -26,20 +69,25 @@ function renderPage(data) {
         picture.setAttribute('id', 'picture');
         image.append(picture);
     }
-    body.append(image);
+    container.append(image);
 
     let explataionText = document.createElement('p');
     explataionText.classList.add('explanation');
     explataionText.innerText = data.explanation;
-    body.append(explataionText);
+    container.append(explataionText);
 
     if (data.copyright) {
         let copyright = document.createElement('span');
         copyright.innerText = data.copyright;
-        body.append(copyright);
+        container.append(copyright);
     }
     body.classList.remove('spinner');
 };
+///}
+
+
+
+
 
 /*document.querySelector('.container').classList.add('spinner');
 
